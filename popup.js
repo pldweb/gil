8aa246f39bad73f8b35b294f52f6f61802ca7555
Muhammad Rivaldi Fanani <76541880+pldweb@users.noreply.github.com>
2026-07-16 09:45:50 +0700

    Remove modal title from popup.js
    Removed the title from the modal header in the popup.

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ $(function() {
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">Informasi</h4>
+                        <h4 class="modal-title"></h4>
                     </div>
                     <div class="modal-body text-center">
                         <a href="${targetUrl}" target="_blank">
