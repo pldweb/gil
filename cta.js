1c90b3e4a8516103117244db27a52cde8eb17587
Muhammad Rivaldi Fanani <76541880+pldweb@users.noreply.github.com>
2026-09-08 13:19:52 +0700

    Fix variable declaration and update string template

--- cta.js
+++ cta.js
@@ -159,14 +159,14 @@ $(document).ready(function () {
     // ==============================
 
     let judul;
-    let desc
+    let desc;
 
      if(!$('.intro-paket-area').length > 0) {
-        judul = `Temukan Paket ${slugpaket} Terbaik untuk Anda`;
+        judul = `Temukan Paket ${slugPaket} Terbaik untuk Anda`;
          desc = "Pilih jadwal keberangkatan yang sesuai, lalu konsultasikan kebutuhan perjalanan ibadah Anda bersama tim Arsytour";
      } else {
          judul = "Masih Ada yang Ingin Ditanyakan?";
-         desc = "<strong>Tim Arsytour</strong> siap membantu menjelaskan detail paket, pilihan kamar, jadwal keberangkatan, serta proses pendaftaran sesuai kebutuhan Anda."
+         desc = "<strong>Tim Arsytour</strong> siap membantu menjelaskan detail paket, pilihan kamar, jadwal keberangkatan, serta proses pendaftaran sesuai kebutuhan Anda.";
      }
 
     var ctaHtml = `
